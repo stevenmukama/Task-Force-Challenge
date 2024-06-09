a2998c70d864764bc069d66f823ef0879f8ddf7c
Mukama Steven <stevenmukama1@gmail.com>
2024-06-09 12:13:32 +0200

    ft add styles and javascript

---
+++ index.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const gallery = document.getElementById('gallery');
+
+	const images = [
+		{ src: 'assets/foxAnimal.png', alt: 'fennec fox', country: 'India' },
+		{
+			src: 'assets/whaleAnimal.png',
+			alt: 'humpback whale',
+			country: 'South Africa',
+		},
+		{
+			src: 'assets/baboonAnimal.png',
+			alt: 'common brown baboon',
+			country: 'South Africa',
+		},
+		{
+			src: 'assets/deerAnimal.png',
+			alt: 'spotted deer',
+			country: 'Amazon',
+		},
+	];
+
+	const createImageContainer = ({ src, alt, country }) => {
+		const container = document.createElement('div');
+		container.className = 'image-container';
+
+		const image = document.createElement('img');
+		image.src = src;
+		image.alt = alt;
+
+		const textOverlay = document.createElement('div');
+		textOverlay.className = 'text-overlay';
+		textOverlay.innerHTML = `<div>${alt}</div><div>${country}</div>`;
+
+		container.appendChild(image);
+		container.appendChild(textOverlay);
+
+		return container;
+	};
+
+	const imageContainers = images.map(createImageContainer);
+	imageContainers.forEach((container) => gallery.appendChild(container));
+});
