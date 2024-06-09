7a05502186cdc970afe89fb7bfdc5e11bf12ff63
Mukama Steven <stevenmukama1@gmail.com>
2024-06-09 20:19:39 +0200

    ft add hover interaction

--- index.js
+++ index.js
@@ -30,7 +30,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
 		const textOverlay = document.createElement('div');
 		textOverlay.className = 'text-overlay';
-		textOverlay.innerHTML = `<div class="animal-description" >${alt}</div><div class="animal-location">${country}</div>`;
+		textOverlay.innerHTML = `
+			<div class="animal-description">${alt}</div>
+			<div class="animal-location">${country}</div>
+			<div class="know-more">
+				Know more
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+				</svg>
+			</div>`;
 
 		container.appendChild(image);
 		container.appendChild(textOverlay);
